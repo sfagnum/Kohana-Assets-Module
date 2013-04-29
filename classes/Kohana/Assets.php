d74199feefba8dc293abd6d51802c6de8c951ac1
sfagnum <sfagnum007@gmail.com>
2013-04-29 20:43:19 +0400

    Kohana code style

--- classes/Kohana/Assets.php
+++ classes/Kohana/Assets.php
@@ -9,10 +9,10 @@
 
 abstract class Kohana_Assets
 {
-    const SCRIPTS = 1;
-    const STYLES = 2;
+    const SCRIPTS = 'scripts';
+    const STYLES = 'styles';
     const INLINEJS = 3;
-    const INLINECSS = 3;
+    const INLINECSS = 4;
     const ALL = 5;
     /**
      * @var
@@ -21,7 +21,7 @@ abstract class Kohana_Assets
     /**
      * @var
      */
-    private $_namespacesList;
+    private $_namespaces_list;
     /**
      * @var
      */
@@ -49,10 +49,11 @@ abstract class Kohana_Assets
      */
     public static function instance($namespace = NULL)
     {
-        if (!isset(self::$_instance)) {
+        if (! isset(self::$_instance))
+        {
             self::$_instance = new Assets();
         }
-        return self::$_instance->_setNamespace($namespace);
+        return self::$_instance->_set_namespace($namespace);
     }
 
     /**
@@ -60,24 +61,34 @@ abstract class Kohana_Assets
      *
      * @return Assets
      */
-    private function _setNamespace($namespace)
+    private function _set_namespace($namespace)
     {
-        $this->_namespacesList = isset($this->_namespacesList) ? $this->_namespacesList : array();
+        $this->_namespaces_list = isset($this->_namespaces_list) ? $this->_namespaces_list : array();
 
-        if (!empty($namespace) && !is_null($namespace)) {
-            if (array_key_exists($namespace, $this->_namespacesList)) {
-                self::$scripts = $this->_namespacesList[$namespace]['scripts'];
-                self::$styles = $this->_namespacesList[$namespace]['styles'];
-            } else {
-                $this->_namespacesList[$namespace] = array('styles' => array(),
-                                                           'scripts'=> array());
-                self::$priority[$namespace] = array('styles' => 0,
-                                                    'scripts'=> 0);
+        if (! empty($namespace))
+        {
+            if (array_key_exists($namespace, $this->_namespaces_list))
+            {
+                self::$scripts = $this->_namespaces_list[$namespace][self::SCRIPTS];
+                self::$styles = $this->_namespaces_list[$namespace][self::STYLES];
+            }
+            else
+            {
+                $this->_namespaces_list[$namespace] = array(
+                    self::STYLES  => array(),
+                    self::SCRIPTS => array()
+                );
+                self::$priority[$namespace] = array(
+                    self::STYLES  => 0,
+                    self::SCRIPTS => 0
+                );
                 self::$scripts = array();
                 self::$styles = array();
             }
             self::$namespace = $namespace;
-        } else {
+        }
+        else
+        {
             self::$namespace = 'defaultNamespace';
         }
         return $this;
@@ -86,65 +97,83 @@ abstract class Kohana_Assets
     /**
      * Bind scripts to current namespace
      *
-     * @param        $scripts
+     * @param mixed  $scripts  can be array or string
      * @param string $directory
+     * @param int    $priority
      *
      * @return Assets
      */
-    public function addScripts($scripts, $directory = '', $priority = 0)
+    public function add_scripts($scripts, $directory = '', $priority = 0)
     {
-        if (empty($scripts)) {
+        if (empty($scripts))
+        {
             return $this;
         }
-        $this->_addAssetsToCurrentNamespace('scripts', $scripts, $directory, $priority);
+        $this->_add_assets_to_namespace(self::SCRIPTS, $scripts, $directory, $priority);
         return $this;
     }
 
     /**
-     * @param        $styles
+     * @param mixed  $styles
      * @param string $directory
+     * @param int    $priority
      *
      * @return Assets
      */
-    public function addStyles($styles, $directory = '', $priority = 0)
+    public function add_styles($styles, $directory = '', $priority = 0)
     {
-        if (empty($styles)) {
+        if (empty($styles))
+        {
             return $this;
         }
-        $this->_addAssetsToCurrentNamespace('styles', $styles, $directory, $priority);
+        $this->_add_assets_to_namespace(self::STYLES, $styles, $directory, $priority);
         return $this;
     }
 
     /**
      * @param             $type; scripts or styles
-     * @param  $assets
+     * @param             $assets
      * @param string      $directory
      * @param int         $priority
      *
      * @return mixed
      */
-    private function _addAssetsToCurrentNamespace($type, $assets, $directory, $priority)
+    private function _add_assets_to_namespace($type, $assets, $directory, $priority)
     {
-        if (!$type) {
+        if (! $type)
+        {
             return;
         }
-        $directory = (!empty($directory) && !is_array($directory)) ? $directory : '';
-        if (is_array($assets)) {
-            foreach ($assets as $asset) {
-                if (!empty($asset) && !is_array($asset)) {
-                    $this->_namespacesList[self::$namespace][$type][$directory . $asset] =
-                        self::_getPriority($priority, $type);
-                } elseif (array_key_exists('priority', $asset)) {
-                    $this->_namespacesList[self::$namespace][$type][$directory . $asset] = $asset['priority'];
+        $directory = (! empty($directory) AND ! is_array($directory)) ? $directory : '';
+        if (is_array($assets))
+        {
+            foreach ($assets as $asset)
+            {
+                if (count($asset))
+                {
+                    if (array_key_exists('priority', $asset))
+                    {
+                        $this->_namespaces_list[self::$namespace][$type][$directory.$asset] = $asset['priority'];
+                    }
+                    else
+                    {
+                        $this->_namespaces_list[self::$namespace][$type][$directory.$asset]
+                            = self::_get_priority($priority, $type);
+                    }
+
                 }
-            }
-        } else {
-            if (!empty($assets) && !is_array($assets)) {
-                $this->_namespacesList[self::$namespace][$type][$directory . $assets] =
-                    self::_getPriority($priority, $type);
+
             }
         }
-        asort($this->_namespacesList[self::$namespace][$type]);
+        else
+        {
+            if (! empty($assets) && ! is_array($assets))
+            {
+                $this->_namespaces_list[self::$namespace][$type][$directory.$assets]
+                    = self::_get_priority($priority, $type);
+            }
+        }
+        asort($this->_namespaces_list[self::$namespace][$type]);
     }
 
     /**
@@ -155,7 +184,7 @@ abstract class Kohana_Assets
      *
      * @return Array
      */
-    private static function _getPriority($priority, $type)
+    private static function _get_priority($priority, $type)
     {
         return ($priority !== 0 ? $priority : ++self::$priority[self::$namespace][$type]);
     }
@@ -165,13 +194,14 @@ abstract class Kohana_Assets
      *
      * @return Kohana_Assets
      */
-    public function addInlineJS($code)
+    public function add_inlineJS($code)
     {
-        if (empty($code)) {
+        if (empty($code))
+        {
             return;
         }
-        $wrap = '<script type="text/javascript">' . $code . '</script>';
-        $this->_namespacesList[self::$namespace]['inlineJS'][] = $wrap;
+        $wrap = '<script type="text/javascript">'.$code.'</script>';
+        $this->_namespaces_list[self::$namespace]['inlineJS'][] = $wrap;
         return $this;
     }
 
@@ -180,97 +210,128 @@ abstract class Kohana_Assets
      *
      * @return Kohana_Assets
      */
-    public function addInlineCSS($css)
+    public function add_inlineCSS($css)
     {
-        if (empty($css)) {
+        if (empty($css))
+        {
             return;
         }
-        $wrap = '<style>' . $css . '</style>';
-        $this->_namespacesList[self::$namespace]['inlineCSS'][] = $wrap;
+        $wrap = '<style>'.$css.'</style>';
+        $this->_namespaces_list[self::$namespace]['inlineCSS'][] = $wrap;
         return $this;
     }
 
     /**
      * @return mixed
      */
-    public function getStyles()
+    public function get_styles()
     {
-        return $this->_namespacesList[self::$namespace]['styles'];
+        return $this->_namespaces_list[self::$namespace][self::STYLES];
     }
 
     /**
      * @return Array
      */
-    public function getScripts()
+    public function get_scripts()
     {
-        return $this->_namespacesList[self::$namespace]['scripts'];
+        return $this->_namespaces_list[self::$namespace][self::SCRIPTS];
     }
-
 
 
     /**
      * Render assets
-     * @param integer $assetsType Assets::ALL, Assets::STYLES
+     *
+     * @param integer $assetsType Assets::ALL, Assets::STYLES etc.
      */
     public function render($assetsType = NULL)
     {
         $assetsType = is_null($assetsType) ? Assets::ALL : $assetsType;
-        switch ($assetsType) {
-            case Assets::SCRIPTS:
-                $this->_renderScripts();
-                break;
-            case Assets::STYLES:
-                $this->_renderStyles();
-                break;
-            case Assets::INLINEJS:
-                $this->_renderInlineJS();
-                break;
-            case Assets::INLINECSS:
-                $this->_renderInlineCSS();
-                break;
-            default:
-                $this->_renderStyles();
-                $this->_renderScripts();
-                $this->_renderInlineJS();
-                $this->_renderInlineCSS();
-                break;
-        }
+        echo $this->_compile_all($assetsType);
     }
 
-    private function _renderScripts()
+    /**
+     * Render assets
+     *
+     * @param integer $assetsType Assets::ALL, Assets::STYLES
+     */
+    private function _compile_all($assetsType)
     {
-        foreach ($this->getScripts() as $script=> $priority) {
-            if (!empty($script)) {
-                echo HTML::script($script) . "\n";
-            }
+        $html = NULL;
+
+        switch ($assetsType)
+        {
+        case self::SCRIPTS:
+            $html .= $this->_render_scripts();
+            break;
+        case self::STYLES:
+            $html .= $this->_render_styles();
+            break;
+        case self::INLINEJS:
+            $html .= $this->_render_inlineJS();
+            break;
+        case self::INLINECSS:
+            $html .= $this->_render_inlineCSS();
+            break;
+        default:
+            $html .= $this->_render_styles();
+            $html .= $this->_render_scripts();
+            $html .= $this->_render_inlineJS();
+            $html .= $this->_render_inlineCSS();
+            break;
         }
+        return $html;
     }
 
-    private function _renderStyles()
+    private function _render_scripts()
     {
-        foreach ($this->getStyles() as $style=> $priority) {
-            if (!empty($style)) {
-                echo HTML::style($style) . "\n";
+        $html = '';
+        foreach ($this->get_scripts() as $script => $priority)
+        {
+            if (! empty($script))
+            {
+                $html .= HTML::script($script)."\n";
             }
         }
+        return $html;
     }
 
-    private function _renderInlineJS()
+    private function _render_styles()
     {
-        if (isset($this->_namespacesList[self::$namespace]['inlineJS'])) {
-            foreach ($this->_namespacesList[self::$namespace]['inlineJS'] as $inlineScript) {
-                echo ($inlineScript) . "\n";
+        $html = '';
+        foreach ($this->get_styles() as $style => $priority)
+        {
+            if (! empty($style))
+            {
+                $html .= HTML::style($style)."\n";
             }
         }
+        return $html;
     }
 
-    private function _renderInlineCSS()
+    private function _render_inlineJS()
     {
-        if (isset($this->_namespacesList[self::$namespace]['inlineCSS'])) {
-            foreach ($this->_namespacesList[self::$namespace]['inlineCSS'] as $inlineStyle) {
-                echo ($inlineStyle) . "\n";
+        $html = '';
+        if (isset($this->_namespaces_list[self::$namespace]['inlineJS']))
+        {
+            foreach ($this->_namespaces_list[self::$namespace]['inlineJS'] as $inlineScript)
+            {
+                $html .= ($inlineScript)."\n";
             }
         }
+        return $html;
+    }
+
+    private function _render_inlineCSS()
+    {
+        $html = '';
+        if (isset($this->_namespaces_list[self::$namespace]['inlineCSS']))
+        {
+            foreach ($this->_namespaces_list[self::$namespace]['inlineCSS'] as $inlineStyle)
+            {
+                $html .= ($inlineStyle)."\n";
+            }
+        }
+        return $html;
     }
 
     function __toString()
